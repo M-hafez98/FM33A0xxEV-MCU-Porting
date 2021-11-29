e04c48cf3ab4b5b565d6dd97a162d8fe62773bd3
Mohamed Hafez <muhammedhafez98@gmail.com>
2021-11-29 11:33:08 +0200

    BSTIM with configurable code

---
+++ BSTIM_interface.c
@@ -0,0 +1,69 @@
+#include "BSTIM_interface.h"
+
+
+void BSTIM_gInit(void) {
+	uint32_t temp, sysclk_hz, BSTIM_pres = 0, ARR_value = 0;
+	CMU_PERCLK_SetableEx(BSTIMCLK, ENABLE);  //Peripheral bus clock enable, APB CLK
+	CMU_OPCCR2_BSTCKS_Set(CMU_OPCCR2_BSTCKS_SYSCLK);//Select working clock source
+	CMU_OPCCR2_BSTCKE_Setable(ENABLE);	 //Working clock source enable
+    BSTIM_CR1_CEN_Setable(DISABLE); // disable the timer
+    BSTIM_ISR_UIF_Clr();
+    NVIC_DisableIRQ(BSTIM_IRQn);
+
+	/* system clock source */
+	temp = CMU->SYSCLKCR & SYS_CLK_SRC_MASK;
+	if(temp == CMU_SYS_CLK_RCHF) {
+		sysclk_hz = RCHF;
+	}
+	else if(temp == CMU_SYS_CLK_XTHF) {
+		sysclk_hz = XTHF;	
+	}
+	else if(temp == CMU_SYS_CLK_PLLH) {
+		sysclk_hz = PLLH;	
+	}
+	else if(temp == CMU_SYS_CLK_LSCLK) {
+		sysclk_hz = LSCLK;	
+	}
+	else {
+		sysclk_hz = RCHF;
+	}
+	/* APB prescaler, assume AHB is sysclk without prescaler */
+	temp = CMU->SYSCLKCR & APB_PRES_MASK;
+	if(temp == APB_PRES_DIV_BY_2) {
+		sysclk_hz /= 2;
+	}
+	else if(temp == APB_PRES_DIV_BY_4){
+		sysclk_hz /= 4;
+	}
+	else if(temp == APB_PRES_DIV_BY_8){
+		sysclk_hz /= 8;
+	}
+	else if(temp == APB_PRES_DIV_BY_16){
+		sysclk_hz /= 16;
+	}
+	else {
+		/*sysclk_hz doesn't change*/
+	}
+	/* timer reload value at pres = 0 */
+	ARR_value = (uint32_t) ((float)(10 / 1000.0) * ( (sysclk_hz) / (BSTIM_pres+1) )) - (uint32_t)1;
+//	while(ARR_value > 4294967295UL) { 
+//		BSTIM_pres++;
+//		ARR_value = (uint32_t) ((float)(10 / 1000.0) * ( (sysclk_hz) / (BSTIM_pres+1) )) - (uint32_t)1;
+//	}
+	BSTIM_PSCR_Write(BSTIM_pres);
+	BSTIM_ARR_Write(ARR_value);
+	NVIC_SetPriority(BSTIM_IRQn ,2);//Interrupt priority configuration 
+	NVIC_EnableIRQ(BSTIM_IRQn );
+	BSTIM_IER_UIE_Setable(ENABLE); //Update interrupt enable
+}
+void BSTIM_gStart(void){
+	BSTIM_CR1_CEN_Setable(ENABLE); //Counter enable
+}
+void BSTIM_gStop(void){
+	BSTIM_CR1_CEN_Setable(DISABLE); //Counter enable
+}
+
+void BSTIM_IRQHandler(void)
+{   
+	
+}
---
+++ BSTIM_interface.h
@@ -0,0 +1,33 @@
+#ifndef __BSTIM_INTERFACE_H__
+#define __BSTIM_INTERFACE_H__
+
+#include "define_all.h"
+
+/*clock sources*/
+#define SYS_CLK_SRC_MASK	(0x7)
+#define CMU_SYS_CLK_RCHF	(0x0) 
+#define CMU_SYS_CLK_XTHF	(0x1) 
+#define CMU_SYS_CLK_PLLH	(0x2) 
+#define CMU_SYS_CLK_LSCLK	(0x3) 
+/*clocks value*/
+#define RCHF 				(8000000)
+#define XTHF 				(16000000)
+#define PLLH 				(32000000)
+#define LSCLK 				(32000)
+/*prescaller values*/
+#define APB_PRES_MASK		(0x70000)
+#define APB_PRES_DIV_BY_2	(0x4)//100
+#define APB_PRES_DIV_BY_4	(0x5)//101
+#define APB_PRES_DIV_BY_8	(0x6)//100
+#define APB_PRES_DIV_BY_16	(0x7)//100
+#define APB_PRES_DIV_BY_1	(0x3)//0xx
+
+
+
+void BSTIM_gInit(void);
+void BSTIM_gStart(void);
+void BSTIM_gStop(void);
+
+
+
+#endif
